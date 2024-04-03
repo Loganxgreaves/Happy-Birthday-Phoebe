df24aa75de1861cec414dc540bcf19ae9c168553
Loganxgreaves <111512664+Loganxgreaves@users.noreply.github.com>
2024-04-03 21:04:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
